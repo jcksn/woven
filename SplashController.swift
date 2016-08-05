b7455d6d5e4dda074dc8a461994f6bee56d56bd7
jcksn <hi@jacksonfall.com>
2016-08-05 14:10:20 -0500

    cleanup

--- SplashController.swift
+++ SplashController.swift
@@ -16,14 +16,13 @@ class SplashController: UIViewController, UICollectionViewDelegate, UICollection
     @IBOutlet weak var logoView: UIView!
     @IBOutlet weak var logoViewWidth: NSLayoutConstraint!
     @IBOutlet weak var logoViewVertical: NSLayoutConstraint!
-    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var collection: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var startButton: UIButton!
     
     //  Variables
     var pageTitles = [String]()
-    var pageImages = [String]()
+//    var pageImages = [String]()
     
     //  Status Bar
     override func prefersStatusBarHidden() -> Bool {
@@ -33,8 +32,8 @@ class SplashController: UIViewController, UICollectionViewDelegate, UICollection
     //  Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        pageTitles = ["Welcome to Woven", "Sexy Meditation, Flavio Approved", "Make America Great Again"]
-        pageImages = ["meditate", "flavio", "america"]
+        pageTitles = ["Tantric Sex Medidtation", "Two, Five, and Fifteen minute meditations curated for you", "Get Started"]
+//        pageImages = ["", "", ""]
         
         setupAppearances()
         setupCollection()
@@ -48,11 +47,10 @@ class SplashController: UIViewController, UICollectionViewDelegate, UICollection
     func setupAppearances() {
         logoView.layer.cornerRadius = 50
         logoView.layer.masksToBounds = true
-        startButton.kern(3)
+        startButton.kern(2)
         startButton.layer.cornerRadius = 10
         startButton.layer.masksToBounds = true
         startButton.alpha = 0
-        titleLabel.alpha = 0
         pageControl.alpha = 0
     }
     func setupCollection() {
@@ -65,14 +63,13 @@ class SplashController: UIViewController, UICollectionViewDelegate, UICollection
     //  Animations
     func reveal() {
         self.logoViewVertical.constant = -((view.frame.height / 2) - 64)
-        UIView.animateWithDuration(1, delay: 1, options: .CurveEaseOut, animations: {
+        UIView.animateWithDuration(1, delay: 1, options: .CurveEaseInOut, animations: {
             self.view.layoutIfNeeded()
             self.logoView.backgroundColor = UIColor.clearColor()
         }) { (complete) in
-            self.logoViewWidth.constant = 180 + 100
+            self.logoViewWidth.constant = 180
             UIView.animateWithDuration(1, delay: 0, options: .CurveEaseOut, animations: {
                 self.view.layoutIfNeeded()
-                self.titleLabel.alpha = 1
                 self.collection.alpha = 1
                 self.startButton.alpha = 1
                 self.pageControl.alpha = 1
@@ -91,9 +88,9 @@ class SplashController: UIViewController, UICollectionViewDelegate, UICollection
     }
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let splashCell: SplashCell = collectionView.dequeueReusableCellWithReuseIdentifier("SplashCell", forIndexPath: indexPath) as! SplashCell
-        splashCell.imageView.image = UIImage(imageLiteral: pageImages[indexPath.row])
+//        splashCell.imageView.image = UIImage(imageLiteral: pageImages[indexPath.row])
         splashCell.titleLabel.text = pageTitles[indexPath.row]
-        splashCell.titleLabel.kern(3)
+        splashCell.titleLabel.kern(1)
         return splashCell
     }
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
